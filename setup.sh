c3e35dafaf5229d135c52a1ac933b7670a0ed48f
Michael Gardner <gardner.j.michael@gmail.com>
2020-03-26 15:03:41 -0400

    always get the path to this director

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ln -s ~/.vim/vimrc ~/.vimrc
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ln -s $DIR/vimrc ~/.vimrc
 
 git -C ~/.vim submodule init
 git -C ~/.vim submodule update
